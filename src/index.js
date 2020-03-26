57ad11d4811d42aee31b58f2d27dea6aeba0afea
samick17 <boneache@gmail.com>
2020-03-26 17:23:57 +0800

    1. Update exported methods

--- src/index.js
+++ src/index.js
@@ -4,30 +4,14 @@ export * as EventUtils from './utils/EventUtils';
 export * as IOUtils from './utils/IOUtils';
 export * as ObjectUtils from './utils/ObjectUtils';
 /* Event Driven Model & Component */
-export { default as Component } from './components/BaseComponent';
-export { default as EventModel } from './core/EventModel';
+export * as Component from './components/BaseComponent';
+export * as EventModel from './core/EventModel';
 /* Object Management Model */
-export { default as IdGenerator } from './core/IdGenerator';
+export * as IdGenerator from './core/IdGenerator';
 export * as Mathf from './core/Mathf';
-export { default as ObjectManager } from './core/ObjectManager';
-export { default as SortedObjectManager } from './core/SortedObjectManager';
+export * as ObjectManager from './core/ObjectManager';
+export * as SortedObjectManager from './core/SortedObjectManager';
 /* Drag & Drop */
-export { default as Draggable } from './core/Draggable';
-export { default as DropZone } from './core/DropZone';
-export { default as AdvancedDraggable } from './core/AdvancedDraggable';
-
-// export default {
-// 	DOMUtils,
-// 	EventUtils,
-// 	IOUtils,
-// 	ObjectUtils,
-// 	Component,
-// 	EventModel,
-// 	IdGenerator,
-// 	Mathf,
-// 	ObjectManager,
-// 	SortedObjectManager,
-// 	Draggable,
-// 	DropZone,
-// 	AdvancedDraggable
-// };
+export * as Draggable from './core/Draggable';
+export * as DropZone from './core/DropZone';
+export * as AdvancedDraggable from './core/AdvancedDraggable';
