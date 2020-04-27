a885a0845ef7a7103954f3b58650b4c6703575f3
Samick.Hsu <Samick.Hsu@tw.viewsonic.com>
2020-04-27 08:51:36 +0800

    1. Updated version to 1.0.10 2. Added uuid for ObjectUtils

--- src/utils/ObjectUtils.js
+++ src/utils/ObjectUtils.js
@@ -754,3 +754,22 @@ export const getShortText = (text, maxLength) => {
         return text.substring(0, endIndex) + '...';
     }
 };
+/*
+ * @category: static function
+ * @description: Generate uuid
+ * @name: uuid
+ * @returns: {string} - The uuid
+ * @example:
+ * const id = uuid();
+ */
+export const uuid = () => {
+    let d = Date.now();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+      d += performance.now();
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      let r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & (0x3 | 0x8))).toString(16);
+    });
+};
