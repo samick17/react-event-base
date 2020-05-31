f825ad78e9c67af0a7d0d17ff35f54b30943c0d8
samick17 <boneache@gmail.com>
2020-05-31 11:39:08 +0800

    1. Refactored DOMUtils & Added registerUnloadEvents/registerContextMenuEvents functions

--- src/utils/DOMUtils.js
+++ src/utils/DOMUtils.js
@@ -39,112 +39,6 @@ const createImageAdapter = (canvas) => {
 	};
 };
 
-const DOMUtils = {
-	createElement: (innerHtml) => {
-		const template = document.createElement('div');
-		template.innerHTML = innerHtml;
-		return template.firstChild;
-	},
-	registerEvent: (element, name, callback) => {
-		element.addEventListener(name, callback);
-		return () => {
-			element.removeEventListener(name, callback);
-		};
-	},
-	htmlToImage: async (innerHtml, scale=1) => {
-		const html2canvas = (await import('html2canvas')).default;
-		const element = DOMUtils.createElement(innerHtml);
-		Object.assign(element.style, {
-			position: 'absolute',
-			'z-index': -1000,
-			left: '-999999px',
-			top: '-999999px'
-		});
-		document.body.append(element);
-		const canvas = await html2canvas(element, {
-			backgroundColor: null,
-			scale,
-			logging: false
-		});
-		element.remove();
-		return createImageAdapter(canvas);
-	},
-	svgToImage: (svgText, options) => {
-		options = options || {};
-		const MaxWidth = options.MaxWidth || 400;
-		const MaxHeight = options.MaxHeight || 400;
-		const canvas = document.createElement('canvas');
-		const ctx = canvas.getContext('2d');
-		// 
-		const reViewBox = /<svg.*viewBox="(.*)".*>/;
-		const reViewBoxResult = reViewBox.exec(svgText);
-		if(reViewBoxResult[0].indexOf('width') < 0 || reViewBoxResult[0].indexOf('height') < 0) {
-			const viewBox = reViewBoxResult[1].split(' ').map(value => parseInt(value));
-			const viewBoxWidth = viewBox[2];
-			const viewBoxHeight = viewBox[3];
-			svgText = svgText.replace(/<(svg.*viewBox=".*".*)>/, (_, i) => {
-				return `<${i} width="${viewBoxWidth}px" height="${viewBoxHeight}px">`;
-			});
-		}
-		//
-		const svg64 = window.btoa(svgText);
-		const b64Start = 'data:image/svg+xml;base64,';
-		const image64 = b64Start + svg64;
-		const img = new Image();
-		return new Promise((resolve, reject) => {
-			img.onload = function() {
-				const aspect = img.width / img.height;
-				const size = DOMUtils.calculateSizeByAspect({
-					width: MaxWidth,
-					height: MaxHeight
-				}, aspect);
-				canvas.width = size.width;
-				canvas.height = size.height;
-				ctx.drawImage(img, 0, 0, img.width, img.height, (canvas.width - size.width) * .5, (canvas.height - size.height) * .5, size.width, size.height);
-				const canvasAdapter = createImageAdapter(canvas);
-				resolve(canvasAdapter);
-			};
-			img.onerror = function(err) {
-				reject(err);
-			};
-			img.src = image64;
-		});
-	},
-	scrollTo: async (element, {fromValue, toValue, animationTime, propertyName}) => {
-		propertyName = propertyName || 'scrollLeft';
-		const startTime = Date.now();
-		const _scrollTo = () => {
-			let deltaTime = Date.now() - startTime;
-			let t = deltaTime / animationTime;
-			let pos = lerp(fromValue, toValue, t);
-			element[propertyName] = pos;
-			if(t < 1) {
-				window.requestAnimationFrame(() => {
-					if(t < 1) {
-						_scrollTo();
-					}
-				});
-			}
-			return t;
-		};
-		_scrollTo();
-	},
-	calculateSizeByAspect: (maxSize, aspect) => {
-		const width = maxSize.width;
-		const height = maxSize.height;
-		let contentWidth = width;
-		let contentHeight = width / aspect;
-		if(contentHeight > height) {
-			contentHeight = height;
-			contentWidth = height * aspect;
-		}
-		return {
-			width: contentWidth,
-			height: contentHeight
-		};
-	}
-};
-
 /*
  * @category: static function
  * @description: Create element from string
@@ -154,7 +48,12 @@ const DOMUtils = {
  * @example:
  * const element = createElement(<div/>);
  */
-export const createElement = DOMUtils.createElement;
+export const createElement = (innerHtml) => {
+	const template = document.createElement('div');
+	template.innerHTML = innerHtml;
+	return template.firstChild;
+};
+
 /*
  * @category: static function
  * @description: Create element from string
@@ -166,7 +65,13 @@ export const createElement = DOMUtils.createElement;
  * @example:
  * const unregisterFn = registerEvent(body, 'click', (e) => {});
  */
-export const registerEvent = DOMUtils.registerEvent;
+export const registerEvent = (element, name, callback) => {
+	element.addEventListener(name, callback);
+	return () => {
+		element.removeEventListener(name, callback);
+	};
+};
+
 /*
  * @category: static function
  * @description: Create element from string
@@ -178,34 +83,25 @@ export const registerEvent = DOMUtils.registerEvent;
  * const imageAdapter = htmlToImage(innerHtml, scale);
  */
  // svgText, options
-export const htmlToImage = DOMUtils.htmlToImage;
-/*
- * @category: static function
- * @description: Create element from string
- * @name: svgToImage
- * @param: {string} svgText - The svg xml text
- * @param: {Object} options - [optional] {MaxWidth, MaxHeight}
- * @returns: {Object} - The ImageAdapter
- * @example:
- * const imageAdapter = svgToImage(svgText, options);
- */
-export const svgToImage = DOMUtils.svgToImage;
-/*
- * @category: static function
- * @description: Create element from string
- * @name: scrollTo
- * @param: {Element} element - The target element
- * @param: {Object} name - {fromValue, toValue, animationTime, propertyName}
- * @returns: No return value
- * @example:
- * const element = document.querySelector('.scroll');
- * const fromValue = 0;
- * const toValue = 800;
- * const animationTime: 300;
- * const propertyName = 'scrollLeft';
- * scrollTo(element, {fromValue, toValue, animationTime, propertyName});
- */
-export const scrollTo = DOMUtils.scrollTo;
+export const = htmlToImage = async (innerHtml, scale=1) => {
+	const html2canvas = (await import('html2canvas')).default;
+	const element = createElement(innerHtml);
+	Object.assign(element.style, {
+		position: 'absolute',
+		'z-index': -1000,
+		left: '-999999px',
+		top: '-999999px'
+	});
+	document.body.append(element);
+	const canvas = await html2canvas(element, {
+		backgroundColor: null,
+		scale,
+		logging: false
+	});
+	element.remove();
+	return createImageAdapter(canvas);
+};
+
 /*
  * @category: static function
  * @description: Create element from string
@@ -222,6 +118,134 @@ export const scrollTo = DOMUtils.scrollTo;
  * const aspect = 16 / 9;
  * const size = calculateSizeByAspect(maxSize, aspect);
  */
-export const calculateSizeByAspect = DOMUtils.calculateSizeByAspect;
+export const calculateSizeByAspect = (maxSize, aspect) => {
+	const width = maxSize.width;
+	const height = maxSize.height;
+	let contentWidth = width;
+	let contentHeight = width / aspect;
+	if(contentHeight > height) {
+		contentHeight = height;
+		contentWidth = height * aspect;
+	}
+	return {
+		width: contentWidth,
+		height: contentHeight
+	};
+};
 
-// export default DOMUtils;
+/*
+ * @category: static function
+ * @description: Create element from string
+ * @name: svgToImage
+ * @param: {string} svgText - The svg xml text
+ * @param: {Object} options - [optional] {MaxWidth, MaxHeight}
+ * @returns: {Object} - The ImageAdapter
+ * @example:
+ * const imageAdapter = svgToImage(svgText, options);
+ */
+export const svgToImage = (svgText, options) => {
+	options = options || {};
+	const MaxWidth = options.MaxWidth || 400;
+	const MaxHeight = options.MaxHeight || 400;
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+	// 
+	const reViewBox = /<svg.*viewBox="(.*)".*>/;
+	const reViewBoxResult = reViewBox.exec(svgText);
+	if(reViewBoxResult[0].indexOf('width') < 0 || reViewBoxResult[0].indexOf('height') < 0) {
+		const viewBox = reViewBoxResult[1].split(' ').map(value => parseInt(value));
+		const viewBoxWidth = viewBox[2];
+		const viewBoxHeight = viewBox[3];
+		svgText = svgText.replace(/<(svg.*viewBox=".*".*)>/, (_, i) => {
+			return `<${i} width="${viewBoxWidth}px" height="${viewBoxHeight}px">`;
+		});
+	}
+	//
+	const svg64 = window.btoa(svgText);
+	const b64Start = 'data:image/svg+xml;base64,';
+	const image64 = b64Start + svg64;
+	const img = new Image();
+	return new Promise((resolve, reject) => {
+		img.onload = function() {
+			const aspect = img.width / img.height;
+			const size = calculateSizeByAspect({
+				width: MaxWidth,
+				height: MaxHeight
+			}, aspect);
+			canvas.width = size.width;
+			canvas.height = size.height;
+			ctx.drawImage(img, 0, 0, img.width, img.height, (canvas.width - size.width) * .5, (canvas.height - size.height) * .5, size.width, size.height);
+			const canvasAdapter = createImageAdapter(canvas);
+			resolve(canvasAdapter);
+		};
+		img.onerror = function(err) {
+			reject(err);
+		};
+		img.src = image64;
+	});
+};
+
+/*
+ * @category: static function
+ * @description: Create element from string
+ * @name: scrollTo
+ * @param: {Element} element - The target element
+ * @param: {Object} name - {fromValue, toValue, animationTime, propertyName}
+ * @returns: No return value
+ * @example:
+ * const element = document.querySelector('.scroll');
+ * const fromValue = 0;
+ * const toValue = 800;
+ * const animationTime: 300;
+ * const propertyName = 'scrollLeft';
+ * scrollTo(element, {fromValue, toValue, animationTime, propertyName});
+ */
+export const scrollTo = async (element, {fromValue, toValue, animationTime, propertyName}) => {
+	propertyName = propertyName || 'scrollLeft';
+	const startTime = Date.now();
+	const _scrollTo = () => {
+		let deltaTime = Date.now() - startTime;
+		let t = deltaTime / animationTime;
+		let pos = lerp(fromValue, toValue, t);
+		element[propertyName] = pos;
+		if(t < 1) {
+			window.requestAnimationFrame(() => {
+				if(t < 1) {
+					_scrollTo();
+				}
+			});
+		}
+		return t;
+	};
+	_scrollTo();
+};
+
+export const registerUnloadEvents = ({onUnload, onBeforeUnload}={}) => {
+	onUnload = onUnload || (() => {});
+	onBeforeUnload = onBeforeUnload || (() => {});
+	const unbindBeforeUnloadEvent = registerEvent(window, 'beforeunload', e => {
+		e.preventDefault();
+		// Chrome requires returnValue to be set.
+		e.returnValue = 'Leave now ?';
+		onBeforeUnload();
+		return false;
+	});
+	const unbindUnloadEvents = registerEvent(window, 'unload', e => {
+		try {
+			onUnload();
+		} catch(err) {
+			console.log(err);
+		}
+	});
+	return () => {
+		unbindBeforeUnloadEvent && unbindBeforeUnloadEvent();
+		unbindUnloadEvents && unbindUnloadEvents();
+	};
+};
+
+export const registerContextMenuEvents = (callback) => {
+	callback = callback || (() => {});
+	return registerEvent(document.body, 'contextmenu', e => {
+		callback(e);
+	});
+};
