c8e05af68a1bcd81236b7952a1eb58e71c022e7b
SamickHsu <samick@SamickHsude-MacBook-Pro.local>
2020-08-23 19:35:15 +0800

    1. Updated version to 1.1.32 2. Added throttle for PerfUtils

--- src/utils/PerfUtils.js
+++ src/utils/PerfUtils.js
@@ -12,3 +12,15 @@ export const debounce = (fn, timeout) => {
 		}, timeout);
 	};
 };
+export const throttle = (callback, limit) => {
+    let waiting = false;
+    return function () {
+        if (!waiting) {
+            callback.apply(this, arguments);
+            waiting = true;
+            setTimeout(function () {
+                waiting = false;
+            }, limit);
+        }
+    };
+}
