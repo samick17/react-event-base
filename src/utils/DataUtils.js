40a27cd2508a9d7e7ce165c9d4b50186efa62793
samick17 <boneache@gmail.com>
2020-11-01 12:15:46 +0800

    1. Added getBy for DataUtils

--- src/utils/DataUtils.js
+++ src/utils/DataUtils.js
@@ -1,3 +1,14 @@
+export const getBy = (data, key) => {
+	const keyArr = key.split('.');
+	let entry = data;
+	for(let i = 0; i < keyArr.length; i++) {
+		entry = entry[keyArr[i]];
+		if(i !== keyArr.length - 1 && typeof entry !== 'object') {
+			return null;
+		}
+	}
+	return entry;
+};
 export const updateWith = (data, key, value) => {
 	const keyArr = key.split('.');
 	let entry = data;
