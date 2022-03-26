9759ad884c93c1fdb4d5cd86bd28beaa57651705
samick17 <boneache@gmail.com>
2022-03-26 16:38:25 +0800

    1. Upgraded rollup/babel devDeps 2. Removed postcss 3. Added cleanup

--- rollup-configs/rollup-config-creator.js
+++ rollup-configs/rollup-config-creator.js
@@ -1,8 +1,8 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
 import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
 
@@ -24,16 +24,14 @@ export default {
       ],
       plugins: [
       external(),
-      postcss({
-        modules: true
-      }),
       url(),
       svgr(),
       babel({
         exclude: 'node_modules/**'
       }),
       resolve(),
-      commonjs()
+      commonjs(),
+      cleanup()
       ]
     };
   }
